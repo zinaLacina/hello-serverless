cf03bf229b880f7c3dd59b18cfd1decf00ba6b5a
zinalacina <zinalacina@gmail.com>
2018-09-07 08:20:25 +0900

    complete project

--- handler.js
+++ handler.js
@@ -1,6 +1,8 @@
-'se strict'
+'use strict'
 
 module.exports.greet = (event,context,callback)=>{
+    
 	console.log(JSON.stringify(event));
+    
 	callback(null,{message:"Hi, this fnction worked"});
 }
